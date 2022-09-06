55330075e0b232549e267a7f5544f95f759ea76c
brenekv.03 <brenekv.03@A321-15>
2022-09-06 12:06:43 +0200

    Nahrání rozhraní

---
+++ TextoveSoubory00/Form1.Designer.cs
@@ -0,0 +1,268 @@
+﻿namespace Soubory
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.button6 = new System.Windows.Forms.Button();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.listBox4 = new System.Windows.Forms.ListBox();
+            this.listBox5 = new System.Windows.Forms.ListBox();
+            this.button10 = new System.Windows.Forms.Button();
+            this.listBox6 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.Location = new System.Drawing.Point(10, 11);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(351, 31);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Vytvoření nového souboru  a zápis do něj";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button2.Location = new System.Drawing.Point(10, 48);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(351, 31);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Zápis na konec souboru";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button3.Location = new System.Drawing.Point(10, 85);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(351, 32);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Čtení souboru po řádcích 1. způsob";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(10, 164);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(169, 184);
+            this.listBox1.TabIndex = 3;
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button4.Location = new System.Drawing.Point(10, 124);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(351, 34);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Čtení souboru po řádcích 2. způsob";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(377, 84);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(163, 212);
+            this.listBox2.TabIndex = 5;
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button5.Location = new System.Drawing.Point(378, 11);
+            this.button5.Margin = new System.Windows.Forms.Padding(2);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(309, 31);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "Čtení souboru po znacích - 1. způsob";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button6.Location = new System.Drawing.Point(377, 46);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(310, 32);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "Čtení souboru po znacích - 2. způsob";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.Location = new System.Drawing.Point(377, 337);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(177, 212);
+            this.listBox3.TabIndex = 8;
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button7.Location = new System.Drawing.Point(10, 363);
+            this.button7.Margin = new System.Windows.Forms.Padding(2);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(351, 31);
+            this.button7.TabIndex = 9;
+            this.button7.Text = "Různý zápis cesty (jen vytvoří soubory)";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button8.Location = new System.Drawing.Point(377, 301);
+            this.button8.Margin = new System.Windows.Forms.Padding(2);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(311, 31);
+            this.button8.TabIndex = 10;
+            this.button8.Text = "OpenFileDialog , SaveFileDialog";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button9.Location = new System.Drawing.Point(728, 11);
+            this.button9.Margin = new System.Windows.Forms.Padding(2);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(342, 31);
+            this.button9.TabIndex = 11;
+            this.button9.Text = "Oprava (přepsání) textového souboru";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // listBox4
+            // 
+            this.listBox4.FormattingEnabled = true;
+            this.listBox4.Location = new System.Drawing.Point(728, 47);
+            this.listBox4.Name = "listBox4";
+            this.listBox4.Size = new System.Drawing.Size(165, 212);
+            this.listBox4.TabIndex = 12;
+            // 
+            // listBox5
+            // 
+            this.listBox5.FormattingEnabled = true;
+            this.listBox5.Location = new System.Drawing.Point(899, 47);
+            this.listBox5.Name = "listBox5";
+            this.listBox5.Size = new System.Drawing.Size(166, 212);
+            this.listBox5.TabIndex = 13;
+            // 
+            // button10
+            // 
+            this.button10.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button10.Location = new System.Drawing.Point(728, 301);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(269, 31);
+            this.button10.TabIndex = 14;
+            this.button10.Text = "Kódování znaků, čeština";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // listBox6
+            // 
+            this.listBox6.FormattingEnabled = true;
+            this.listBox6.Location = new System.Drawing.Point(728, 334);
+            this.listBox6.Name = "listBox6";
+            this.listBox6.Size = new System.Drawing.Size(177, 212);
+            this.listBox6.TabIndex = 15;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1181, 558);
+            this.Controls.Add(this.listBox6);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.listBox5);
+            this.Controls.Add(this.listBox4);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.ListBox listBox4;
+        private System.Windows.Forms.ListBox listBox5;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.ListBox listBox6;
+    }
+}
+
---
+++ TextoveSoubory00/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Soubory
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //Pro práci se soubory v CSharp vždy potřebujeme nějaký stream.
+
+        //Textový soubor je speciální typ souboru, kde jsou znaky uspořádány do řádků (pomocí řídících znaků CR a LF)
+        //Textové soubory můžeme zpracovávat obecně, jako kterýkoliv jiný soubor, ale pokud chceme
+        //zpracovávat text, práci nám usnadní použití speciálních streamů určených právě jen pro textové soubory: 
+        //          StreamWriter 
+        //          StreamReader
+        
+        //Textové soubory lze zpracovávat jedině sekvenčně - od začátku do konce, při zpracování se nelze v souboru vracet.
+        //Proto můžeme do textového souboru buďto zapisovat nebo z něj číst. Nelze obojí současně.
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Pro zápis do textového souboru použijeme třídu StreamWriter
+            //- slouží pro otevření konkrétního textového souboru (pokud neexistuje - vytvoří se)
+            //  a má metody pro zápis do textového souboru (Write a WriteLine) 
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+                        
+               
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //Přečte textový soubor po řádcích 
+            //a každyý přečtený řádek zobrazí v listBox1
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //Přečtené řádky budeme zobrazovat v listbox
+
+            listBox1.Items.Clear();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            
+            
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            //Na rozdíl od řetězce nemůžeme číst znak na konci souboru
+            //Metoda Peek - vrací kód znaku, který je na řadě pro další čtení - zeptáme se, zda je ještě něco ke čtení,
+            //pokud žádný další znak není (konec souboru) - vrátí Peek kód -1
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            //(Vytvoříme prázdné textové soubory)
+
+            //Různé způsoby zápisu cesty: (Nelze jednoduše psát opačné lomítko)
+            //Při programování aplikace používejte skoro vždy relativní cesty!!!
+
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            //Zobrazí vybraný soubor
+            //Soubor vybereme pomocí komponenty OpenFileDialog nebo SaveFileDialog
+            //Tyto dialogy nic neotevírají ani neukládají, jen nám umožní vybrat soubor, ostatní musíme naprogramovat sami
+
+
+
+            //Vyzkoušet jiný způsob obsloužení dialogu!!!!
+
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            //V textovém souboru vybraném pomocí OpenFileDialogu zapíšeme
+            //na konec každého řádku *
+
+            //Textový soubor opravíme tak, že jej celý čteme, opravemé řádky nebo znaky zapisujeme
+            //do pomocného textového souboru.
+            //Oba streamy zavřeme, původní soubor smažeme 
+            //a pomocný soubor přejmenujeme na jméno původního souboru (včetně umístění)
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
